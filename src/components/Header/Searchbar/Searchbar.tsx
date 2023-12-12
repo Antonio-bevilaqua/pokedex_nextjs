49e82dbeec309b5203591a120156d550f3f85ac8
Antonio Bevilaqua <toni_bevila@hotmail.com>
2023-12-11 21:06:05 -0300

    adicionando borda superior na div de busca

--- src/components/Header/Searchbar/Searchbar.tsx
+++ src/components/Header/Searchbar/Searchbar.tsx
@@ -34,7 +34,7 @@ const Searchbar = ({ className = "" }: { className?: string }) => {
                 onChange={onSearchChanged}
             />
             {list.length > 0 && search.length >= 2 && (
-                <div className="searchbar absolute left-0 w-56 rounded-b-lg bg-gray-300 overflow-y-auto max-h-44 dark:bg-gray-900 border-solid border-l-2 border-r-2 border-b-2 border-gray-600 dark:border-sky-200" style={{ zIndex: 60, top: "3.2rem" }}>
+                <div className="searchbar absolute left-0 w-56 rounded-lg bg-gray-300 overflow-y-auto max-h-44 dark:bg-gray-900 border-solid border-2 border-gray-600 dark:border-sky-200" style={{ zIndex: 80, top: "3.2rem" }}>
                     <ul>
                         {list.map((pokemon) => (
                             <a
